f0ddc0833758a705ee63fa173b11de3e4b0a3610
Olivier Chafik <olivier.chafik@gmail.com>
2012-03-22 21:42:01 +0000

    BridJ: prépare pure virtual C++ classes fix

--- src/test/java/org/bridj/CPPTest2.java
+++ src/test/java/org/bridj/CPPTest2.java
@@ -8,6 +8,7 @@ import org.bridj.*;
 import static org.bridj.Pointer.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 @Library("test") 
 public class CPPTest2 {
@@ -54,4 +55,28 @@ public class CPPTest2 {
         
         
     }
+    
+    @Library("test") 
+    public abstract class IVirtual extends CPPObject {
+        public IVirtual() {
+            super();
+        }
+        @Virtual(2)
+        public native int add(int a, int b);
+    }
+    public static native int testIVirtualAdd(Pointer<IVirtual> pVirtual, int a, int b);
+    
+    @Ignore
+    @Test
+ 	public void testPureVirtual() {
+ 		BridJ.register(getClass());
+ 		
+ 		IVirtual v = new IVirtual() {
+ 			@Override
+ 			public int add(int a, int b) {
+ 				return a * 10 + b * 100;	
+ 			}
+ 		};
+ 		assertEquals(210, testIVirtualAdd(pointerTo(v), 1, 2));
+ 	}
 }
