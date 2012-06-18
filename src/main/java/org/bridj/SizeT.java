46abaf2458afa99f661ed3b79340f68d982e942a
Olivier Chafik <olivier.chafik@gmail.com>
2012-06-18 23:52:43 +0100

    BridJ: added SizeT.ZERO and ONE

--- src/main/java/org/bridj/SizeT.java
+++ src/main/java/org/bridj/SizeT.java
@@ -13,12 +13,19 @@ public final class SizeT extends AbstractIntegral {
     
 	public static final int SIZE = Platform.SIZE_T_SIZE;
 	
+	public static final SizeT ZERO = new SizeT(0), ONE = new SizeT(1);
+	
 	private static final long serialVersionUID = 1547942367767922396L;
+	
 	public SizeT(long value) {
 		super(value);
     }
     
     public static SizeT valueOf(long value) {
-    		return new SizeT(value);
+    	if (value == 0)
+    		return ZERO;
+    	if (value == 1)
+    		return ONE;
+    	return new SizeT(value);
     }
 }
