7830e54a8828b2c34a0022edb7bc63cfb87e3354
Olivier Chafik <olivier.chafik@gmail.com>
2012-07-21 14:01:16 +0100

    BridJ: fixed major Win64 bug in assembler optimizations (didn't leave a register spill zone as needed, which could lead in crashes when calling functions that needed to reference their arguments by copying them into the spill zone)

--- src/main/cpp/bridj/RawNativeForwardCallback_WIN64_masm.asm
+++ src/main/cpp/bridj/RawNativeForwardCallback_WIN64_masm.asm
@@ -11,6 +11,7 @@ CTX_thunk       =    0
 CTX_handler     =   24
 CTX_userdata    =   32
 DCCallback_size =   40
+STACK_offset    =   48
 ;ZERO_IREG_ISMS	=	
 
 dcRawCallAdapterSkipTwoArgs64 PROC ; EXPORT
@@ -20,22 +21,29 @@ dcRawCallAdapterSkipTwoArgs64 PROC ; EXPORT
   	push     rbp
 	mov      rbp, rsp		; aligns stack to 16
 	push rbx
-	;push 0
     push rsi
+	sub rsp, 4 * 8			; push spill space for register args
     
 	; Copying the 4 first arguments
 	mov rcx, r8
 	mov rdx, r9
-	mov r8, [rbp + 8 + 8]
-	mov r9, [rbp + 16 + 8]
+	mov r8, [rbp + STACK_offset]
+	mov r9, [rbp + STACK_offset + 8]
+  	
+  	;mov rcx, rbp
+	;add rcx, STACK_offset
+	;mov r8, [rcx]
+	;mov r9, [rcx + 4]
+	
   	movd xmm0, rcx
   	movd xmm1, rdx
   	movd xmm2, r8
   	movd xmm3, r9
   	
 	call    qword ptr[rax+CTX_handler]
-		
-	;mov      rsp, rbp
+	
+	add rsp, 4 * 8			; pop spill space
+	
 	pop rsi
     pop rbx
 	pop      rbp
