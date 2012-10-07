3cb211a6fbffbe06e347f1e697636da5bc0e4fbb
Olivier Chafik <olivier.chafik@gmail.com>
2012-10-07 16:46:12 +0100

    BridJ: prepare more objc block works (issue #192)

--- src/main/cpp/bridj/ObjCBlocks.m
+++ src/main/cpp/bridj/ObjCBlocks.m
@@ -9,14 +9,29 @@
 http://cocoawithlove.com/2009/10/how-blocks-are-implemented-and.html
 http://www.opensource.apple.com/source/libclosure/libclosure-38/BlockImplementation.txt
 */
-typedef struct Block_literal {
+
+enum {
+    BLOCK_HAS_COPY_DISPOSE =  (1 << 25),
+    BLOCK_HAS_CTOR =          (1 << 26),
+    BLOCK_IS_GLOBAL =         (1 << 28),
+    BLOCK_HAS_DESCRIPTOR =    (1 << 29),
+};
+
+typedef struct _block_descriptor {
+	unsigned long int reserved;
+	unsigned long int block_size;
+	void (*copy_helper)(void *dst, void *src);
+	void (*dispose_helper)(void *src);
+} _block_descriptor;	
+
+typedef struct _block_literal {
 	void* isa;
 	int flags;
 	int reserved; 
 	void (*invoke)(void *, ...);
-	void* descriptor;
-} Block_literal;	
-	
+	struct _block_descriptor* descriptor;
+} _block_literal;	
+
 const void* createObjCBlock() {
 	void (^block)() = ^{
 		// do nothing
@@ -25,12 +40,12 @@ const void* createObjCBlock() {
 }
 jlong Java_org_bridj_objc_ObjCJNI_getObjCBlockFunctionPointer(JNIEnv* env, jclass cl, jlong jblock)
 {
-	Block_literal* block = (Block_literal*)JLONG_TO_PTR(jblock);
+	_block_literal* block = (_block_literal*)JLONG_TO_PTR(jblock);
 	return PTR_TO_JLONG(block->invoke);
 }
 jlong Java_org_bridj_objc_ObjCJNI_createObjCBlockWithFunctionPointer(JNIEnv* env, jclass cl, jlong fptr)
 {
-	Block_literal* block = (Block_literal*)createObjCBlock();
+	_block_literal* block = (_block_literal*)createObjCBlock();
 	block->invoke = JLONG_TO_PTR(fptr);
 	return PTR_TO_JLONG(block);
 }
