61bbf470ce4b6a2a9d05f36a875d4c274296709e
Olivier Chafik <olivier.chafik@gmail.com>
2012-12-25 17:44:59 +0100

    BridJ: turn the main artifact into an OSGi bundle

---
+++ src/main/java/org/bridj/OSGiBundleActivator.java
@@ -0,0 +1,15 @@
+package org.bridj;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class OSGiBundleActivator implements BundleActivator {
+	
+	public void start(BundleContext bundleContext) {
+		//System.out.println("Starting BridJ");
+	}
+	
+	public void stop(BundleContext bundleContext) {
+		//System.out.println("Stopping BridJ");
+	}
+}
