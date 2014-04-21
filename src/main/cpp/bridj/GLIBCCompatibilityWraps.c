bcfc6424b97198972f292bada7b75410412b0657
Olivier Chafik <olivier.chafik@gmail.com>
2014-04-21 23:20:52 +0100

    BridJ: turn compat wraps to a macro

--- src/main/cpp/bridj/GLIBCCompatibilityWraps.c
+++ src/main/cpp/bridj/GLIBCCompatibilityWraps.c
@@ -1,13 +1,18 @@
-#if !defined(__ANDROID__)
+#ifndef __ANDROID__
 
 #include <string.h>
 
-void *__memcpy_glibc_2_2_5(void *, const void *, size_t);
-asm(".symver __memcpy_glibc_2_2_5, memcpy@GLIBC_2.2.5");
+#define WRAP_SUB(name, return_keyword, return_type, arg_defs, args, underscored_version, dotted_version) \
+  return_type __ ## name ## _ ## underscored_version arg_defs; \
+  __asm__(".symver __" #name "_" #underscored_version ", " #name "@" #dotted_version); \
+  return_type __wrap_ ## name arg_defs { \
+    return_keyword __ ## name ## _ ## underscored_version args; \
+  }
 
-void *__wrap_memcpy(void *dest, const void *src, size_t n)
-{
-    return __memcpy_glibc_2_2_5(dest, src, n); 
-}
+#define WRAP(name, return_keyword, return_type, arg_defs, args) \
+  WRAP_SUB(name, return_keyword, return_type, arg_defs, args, \
+    glibc_2_2_5, GLIBC_2.2.5)
+
+WRAP(memcpy, return, void*, (void *dest, const void *src, size_t n), (dest, src, n));
 
 #endif
