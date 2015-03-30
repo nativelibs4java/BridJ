a4e763236218ef0ccc90948035ee6b29bf2ed88e
Olivier Chafik <olivier.chafik@gmail.com>
2015-03-30 23:06:47 +0100

    oops, typo

--- src/test/java/org/bridj/JAWTTest.java
+++ src/test/java/org/bridj/JAWTTest.java
@@ -43,7 +43,7 @@ public class JAWTTest {
 	@Test
 	public void testWindowPeer() throws Exception {
     if (Platform.isMacOSX() ||
-        System.getProperty("java.version)").matches("1\\.6\\..*")) {
+        System.getProperty("java.version").matches("1\\.6\\..*")) {
       // Oracle Java and jawt: it's complicated.
       // See http://forum.lwjgl.org/index.php?topic=4326.0
       // OpenJDK 6 + Linux seem problematic too.
