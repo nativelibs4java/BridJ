c7ae5f63ae9806e4d118ac311d83575e93a3bc13
Christian Trimble <christian.trimble@meltmedia.com>
2017-01-19 20:49:23 -0700

    removed tests comparing origin and new resolution code.

--- src/test/java/org/bridj/StructFieldDeclarationTest.java
+++ src/test/java/org/bridj/StructFieldDeclarationTest.java
@@ -27,42 +27,4 @@ public class StructFieldDeclarationTest {
   public static class NestedStructOfBasicFieldStruct
   extends GenericNestedStruct<BasicFieldStruct> {}
 
-  @Test
-  public void testDifferences() {
-    List<StructFieldDeclaration> legacy = StructFieldDeclaration.listFields(BasicFieldStruct.class);
-    List<StructFieldDeclaration> resolved = StructFieldDeclaration.listFields(BasicFieldStruct.class);
-    
-    assertThat(resolved.size(), equalTo(legacy.size()));
-  }
-
-  @Test
-  public void testStaticNestedStruct() {
-    List<StructFieldDeclaration> legacy = StructFieldDeclaration.listFields(StaticNestedStruct.class);
-    List<StructFieldDeclaration> resolved = StructFieldDeclaration.listFields(StaticNestedStruct.class);
-    
-    assertThat(resolved.size(), equalTo(legacy.size()));
-    
-    for( int i = 0; i < legacy.size(); i++ ) {
-      assertThat(resolved.get(i).declaringClass, equalTo(legacy.get(i).declaringClass));
-      assertThat(resolved.get(i).valueClass, equalTo(legacy.get(i).valueClass));
-    }
-  }
-
-  @Test
-  public void testGenericNestedStruct() {
-    List<StructFieldDeclaration> legacy = StructFieldDeclaration.listFields(NestedStructOfBasicFieldStruct.class);
-    List<StructFieldDeclaration> resolved = StructFieldDeclaration.listFields(NestedStructOfBasicFieldStruct.class);
-    
-    assertThat(resolved.size(), equalTo(legacy.size()));
-    
-    for( int i = 0; i < legacy.size(); i++ ) {
-      assertThat("declaringClass", resolved.get(i).declaringClass, equalTo(legacy.get(i).declaringClass));
-      assertThat("legacyValueClass", legacy.get(i).valueClass, equalTo(StructObject.class));
-      assertThat("resolvedValueClass", resolved.get(i).valueClass, equalTo(BasicFieldStruct.class));
-      assertThat("index", resolved.get(i).index, equalTo(legacy.get(i).index));
-      assertThat("setter", resolved.get(i).setter, equalTo(legacy.get(i).setter));
-      assertThat("index", resolved.get(i).unionWith, equalTo(legacy.get(i).unionWith));
-      
-    }
-  }
 }
